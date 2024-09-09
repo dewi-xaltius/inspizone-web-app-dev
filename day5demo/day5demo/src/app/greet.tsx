5604e0f23580d45e27aa9c07c021d79538dedcce
dewi-xaltius <153882863+dewi-xaltius@users.noreply.github.com>
2024-09-09 20:48:37 +0800

    Delete day5demo/day5demo directory

--- day5demo/day5demo/src/app/greet.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function greet() {
-  return (
-    <div><h1> Welcome Archana, you have 10 unread messages</h1></div>
-  )
-}
-
-export default greet
--- day5demo/day5demo/src/app/profile/types.ts
+++
@@ -1,6 +0,0 @@
-export interface UserProfile{
-    id:number;
-    name:string;
-    email:string;
-    age:number;
-}
